2fe889876b7bab1c1f2c85a08ccbe33b03207a34
Adrian Chavarria <adrichavamo@gmail.com>
2020-07-09 04:38:50 -0600

    New Plot 2 and changes in plot1

--- Plot1.R
+++ Plot1.R
@@ -5,10 +5,9 @@
 #####################################################
 
 #Read data file
-
 power <- read.table("household_power_consumption.txt",skip=1,sep=";")
-#Naming columns
 
+#Naming columns
 names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 
 #Subsetting power consumption data
